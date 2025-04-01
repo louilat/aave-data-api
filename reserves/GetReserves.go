262a85453abf291031fb6891f6f71b640c07b869
louislat <lmm.latournerie@gmail.com>
2025-04-01 09:32:31 +0200

    remove weekday censorship

--- reserves/GetReserves.go
+++ reserves/GetReserves.go
@@ -24,7 +24,7 @@ func GetReserves(c *gin.Context) {
 		c.IndentedJSON(http.StatusBadRequest, gin.H{"message": err})
 	}
 
-	if dt.Weekday() != 1 {
+	if dt.Weekday() > 7 {
 		c.IndentedJSON(http.StatusNotFound, gin.H{"message": "No content found"})
 	} else {
 		key := "aave-api-datasource/daily-users-balances/users_balances_snapshot_date=" + dt.String()[:10] + "/reserves_data.json"
--- users/GetUsersBalances.go
+++ users/GetUsersBalances.go
@@ -24,7 +24,7 @@ func GetUsersBalances(c *gin.Context) {
 		c.IndentedJSON(http.StatusBadRequest, gin.H{"message": err})
 	}
 
-	if dt.Weekday() != 1 {
+	if dt.Weekday() > 7 {
 		c.IndentedJSON(http.StatusNotFound, gin.H{"message": "No content found"})
 	} else {
 		key := "aave-api-datasource/daily-users-balances/users_balances_snapshot_date=" + dt.String()[:10] + "/users_balances.json"
