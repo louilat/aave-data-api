6d5f3ac0e923269eceb931e89ffa2d8e8213551b
louislat <lmm.latournerie@gmail.com>
2025-04-01 09:30:20 +0200

    add indexs + liq threshold to reserves data type

--- tps/Types.go
+++ tps/Types.go
@@ -9,15 +9,18 @@ type MinioCreds struct {
 }
 
 type ReserveData struct {
-	Name                string  `json:"name"`
-	LastUpdateTimestamp int64   `json:"lastUpdateTimestamp"`
-	Decimals            int64   `json:"decimals"`
-	Asset               string  `json:"underlyingAsset"`
-	PriceUSD            float64 `json:"underlyingTokenPriceUSD"`
-	LiquidityRate       string  `json:"liquidityRate"`
-	VariableBorrowRate  string  `json:"variableBorrowRate"`
-	AvailableLiquidity  string  `json:"availableLiquidity"`
-	TotalVariableDebt   string  `json:"totalScaledVariableDebt"`
+	Name                 string  `json:"name"`
+	LastUpdateTimestamp  int64   `json:"lastUpdateTimestamp"`
+	Decimals             int64   `json:"decimals"`
+	Asset                string  `json:"underlyingAsset"`
+	PriceUSD             float64 `json:"underlyingTokenPriceUSD"`
+	LiquidityRate        string  `json:"liquidityRate"`
+	VariableBorrowRate   string  `json:"variableBorrowRate"`
+	LiquidityIndex       string  `json:"liquidityIndex"`
+	VariableBorrowIndex  string  `json:"variableBorrowIndex"`
+	LiquidationThreshold int64   `json:"reserveLiquidationThreshold"`
+	AvailableLiquidity   string  `json:"availableLiquidity"`
+	TotalVariableDebt    string  `json:"totalScaledVariableDebt"`
 }
 
 type UserBalanceData struct {
